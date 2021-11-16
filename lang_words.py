e898b241bcab34e5a5de860c02112ff0e55bb472
AYehia0 <ahmedyehia611@gmail.com>
2021-11-16 07:29:10 +0200

    All the language words and grammar in one place

---
+++ lang_words.py
@@ -0,0 +1,20 @@
+# Token types 
+TOKEN_TYPES = {
+    "*":"MUL",
+    "/":"DIV",
+    "+":"ADD",
+    "-":"SUB",
+    "(":"L_P",
+    ")":"R_P",
+}
+
+# Data types 
+DATA_TYPES = {
+    "T_INT" : "INT",
+    "T_FLOAT" : "FLOAT",
+}
+
+# EOF : End of file used to indicate the end
+EOF = "EOF"
+
+DIGITS = "1234567890"
